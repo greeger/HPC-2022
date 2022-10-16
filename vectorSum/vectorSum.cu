bbd6065b9632758d289b182c537cd172ad46640f
greeger <grigoriydeev.rar@gmail.com>
2022-10-16 11:53:09 +0400

    done

--- vectorSum/vectorSum.cu
+++ vectorSum/vectorSum.cu
@@ -4,67 +4,78 @@
 
 using namespace std;
 
-static const int blockSize = 32;
+static const int blockSize = 1024;
+static const int N = 1000000;
 
-double vectorSum_cpu(float* A, float &sum, int N){
+double vectorSum_cpu(float* A, float* sum){
+	float lsum = 0;
     struct timeval start, end;
     gettimeofday(&start, NULL);
     for(int i = 0; i < N; i++)
-      sum += A[i];
+      lsum += A[i];
     gettimeofday(&end, NULL);
+	*sum = lsum;
     return (end.tv_sec - start.tv_sec) + ((double)end.tv_usec - start.tv_usec)/1000000;
 }
 
-__global__ void vs(float* A, float &sum, int N)
+__global__ void vs(float* A, float* sum)
 {
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int x = threadIdx.x;
 	
-	int newN = blockSize * gridDim / 2;
+	if(x >= N) return;
 	
-	__shared__ int r[newN];
-    
+	float lsum = 0;
+	for(int i = x; i < N; i += blockSize)
+		lsum += A[i];
+	
+	__shared__ float r[blockSize];
+	r[x] = lsum;
+	
 	__syncthreads();
 	
-	for (int i = newN; i > 0; i /= 2) {
+	for (int i = blockSize/2; i > 0; i /= 2) {
         if (x < i)
             r[x] += r[x + i];
         __syncthreads();
     }
+	
     if (x == 0)
-        sum = r[0];
- 
-    return;
+        *sum = r[0];
 }
 
-double vectorSum_gpu(float* A, float &sum, int N){
+double vectorSum_gpu(float* A, float* sum){
     dim3 blockDim(blockSize);
-	
-	int newN;
-	if (N <= blockSize) newN = blockSize;
-	while(N > newN) newN *= 2;
-	
-    dim3 gridDim(newN/(blockSize*2));
 
     cudaEvent_t start, end;
     cudaEventCreate(&start);
     cudaEventCreate(&end);
 
+	float* cudaA;
+	cudaMalloc(&cudaA, N * sizeof(float));
+	cudaMemcpy(cudaA, A, N * sizeof(float), cudaMemcpyHostToDevice);
+
+	float* cudaSum;
+	cudaMalloc(&cudaSum, sizeof(float));
+
     float gpu_time = 0.0f;
 	
     cudaEventRecord(start);
 
-    vs <<< gridDim, blockDim >>> (A, sum, N);
+    vs <<< 1, blockDim >>> (cudaA, cudaSum);
 
     cudaEventRecord(end);
     cudaEventSynchronize(end);
 	
     cudaEventElapsedTime(&gpu_time, start, end);
+	
+	cudaMemcpy(sum, cudaSum, sizeof(float), cudaMemcpyDeviceToHost);
+	cudaFree(cudaSum);
+	cudaFree(cudaA);
 
     return gpu_time/1000;
 }
 
 int main() {
-    int N = 1000;
 
     float* A = (float *)malloc(N * sizeof(float));
     float sum_cpu = 0;
@@ -74,9 +85,10 @@ int main() {
     for (int i = 0; i < N; i++)
       A[i] = rand()%10 + 0.1;
 
-    double rez_cpu = vectorSum_cpu(A, sum_cpu, N);
-    double rez_gpu = vectorSum_gpu(A, sum_gpu, N);
+    double rez_cpu = vectorSum_cpu(A, &sum_cpu);
+    double rez_gpu = vectorSum_gpu(A, &sum_gpu);
 
+	cout << "blockSize = " << blockSize << endl;
     cout << "N = " << N << endl;
     cout << "cpu: " << rez_cpu << " sec" << endl;
     cout << "gpu: " << rez_gpu << " sec" << endl;
